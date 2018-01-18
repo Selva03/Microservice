7b8f845a3d4b27cc1539b229da9e96c478f4d334
selva <selva@DESKTOP-08AI07G>
2018-01-18 23:08:36 +0530

    Selva

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -27,22 +27,12 @@ INSERT INTO USER_ROLE (user_id, role_id)
 VALUES (3, 2);
 
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Product1', 'IT Products Line Items', 3, 2500.00);
+VALUES ('Product1', 'Virtual IT Bundle Core', 3, 12500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Product2', 'IT Products Line Items', 9, 3000.00);
+VALUES ('Product2', 'Virtual IT Bundle Cisco', 9, 3000.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Product3', 'IT Products Line Items', 5, 3500.00);
+VALUES ('Product3', 'Virtual IT Bundle NBNCo', 5, 3500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Product4', 'IT Products Line Items', 7, 4500.00);
+VALUES ('Product4', 'Virtual IT Service Bundle', 7, 4500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Product5', 'IT Products Line Items', 3, 6500.00);
-INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Wrist Watch', 'Imported wrist watches from swiss', 800, 2500.00);
-INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Mobile Phone', '3G/4G capability', 700, 45000.00);
-INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Shampoo', 'Head and Shoulders Shampoo', 500, 300.00);
-INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Leather Wallets', 'Imported Leather Wallets from AUS', 1000, 500.00);
-INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Camera', 'Imported Canon camera from USA', 10, 85000.00);
+VALUES ('Product5', 'Virtual IT Network Bundle', 3, 6500.00);
